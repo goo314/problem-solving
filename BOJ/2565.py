a81f2b9d1d525d4b859f14d14384c3046cfc7b77
goo314 <rn0731@g.skku.edu>
2023-01-10 21:58:25 +0900

    solve 2565

---
+++ BOJ/2565.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+arr = []
+for _ in range(n):
+    a, b = map(int, input().split())
+    arr.append((a, b))
+arr = sorted(arr, key=lambda x: x[0])
+
+
+
+dp = [0]*n
+for i in range(n):
+    a, b = arr[i]
+    
+    tmp = 0
+    for j in range(i):
+        if arr[j][1] < b and tmp < dp[j]:
+            tmp = dp[j]
+    dp[i] = tmp + 1
+
+print(n-max(dp))
