58679d93b7f75c68b9e14fa5230420b0433cff27
goo314 <rn0731@g.skku.edu>
2023-11-26 15:03:21 -0800

    [leetcode] solve 1689: min partitions

---
+++ leetcode/1689.py
@@ -0,0 +1,21 @@
+"""LongestPanlindrome
+
+Algorithm : 
+    Greedy
+Level :
+    Medium
+Status :
+    Accepted
+
+Sun Nov 26 15:02:50 PST 2023
+"""
+
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        result = 0
+
+        digits = list(map(int, list(n)))
+
+        result = max(digits)
+
+        return result
