5df8fbeffd6337021dd6e043e301faa924320a45
goo314 <rn0731@g.skku.edu>
2023-11-26 14:55:45 -0800

    [leetcode] solve 2160: minimum sum

---
+++ leetcode/2160.py
@@ -0,0 +1,24 @@
+"""LongestPanlindrome
+
+Algorithm : 
+    Greedy
+Level :
+    Easy
+Status :
+    Accepted
+
+Sun Nov 26 14:54:56 PST 2023
+"""
+
+class Solution:
+    def minimumSum(self, num: int) -> int:
+        result = 0
+        nums = sorted(list(str(num)))
+
+        new1 = nums[0] + nums[2]
+        new2 = nums[1] + nums[3]
+
+        result = int(new1) + int(new2)
+
+        return result
+        
