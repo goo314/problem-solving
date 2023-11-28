1848be39d20a01d4096700b0da7685700b8a5816
goo314 <rn0731@g.skku.edu>
2023-11-27 23:54:57 -0800

    [leetcode] solve 841: can visit all rooms

---
+++ leetcode/841.py
@@ -0,0 +1,36 @@
+"""LongestPanlindrome
+
+Algorithm : 
+    BFS
+Level :
+    Medium
+Status :
+    Accepted
+
+Mon Nov 27 23:54:35 PST 2023
+"""
+
+from collections import deque
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        result = True
+
+        visited = [False] * len(rooms)
+
+        queue = deque()
+
+        start = 0
+        queue.append(start)
+        visited[start] = True
+
+        while queue:
+            x = queue.popleft()
+            for nx in rooms[x]:
+                if not visited[nx]:
+                    visited[nx] = True
+                    queue.append(nx)
+        
+        if False in visited:
+            result = False
+
+        return result
