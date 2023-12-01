7b578e7aeb7ce26fea1a60bda0301ca21c2e5c91
goo314 <rn0731@g.skku.edu>
2023-11-30 23:22:11 -0800

    [leetcode] solve 35: search insert

---
+++ leetcode/35.py
@@ -0,0 +1,21 @@
+"""LongestPanlindrome
+
+Algorithm : 
+    Binary Search
+Level :
+    Easy
+Status :
+    Accepted
+
+Thu Nov 30 23:21:54 PST 2023
+"""
+
+from bisect import bisect_left, bisect_right
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        result = 0
+        left = bisect_left(nums, target)
+        right = bisect_right(nums, target)
+        result = (left+right)//2
+        
+        return result
