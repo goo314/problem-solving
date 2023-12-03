3004e4d16daeb1111e281dd02ef7057b4dd2566f
goo314 <rn0731@g.skku.edu>
2023-12-03 14:50:12 -0800

    [leetcode] solve 338: count bits

---
+++ leetcode/338.py
@@ -0,0 +1,20 @@
+"""LongestPanlindrome
+
+Algorithm : 
+    Dynamic Programming
+Level :
+    Easy
+Status :
+    Accepted
+
+Sun Dec  3 14:49:25 PST 2023
+"""
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        result = []
+
+        for i in range(n+1):
+            result.append(str(bin(i)).count('1'))
+
+        return result
