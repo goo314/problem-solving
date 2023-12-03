5375c93c909af894ca9ca9dc62addf136e5bb0fe
goo314 <rn0731@g.skku.edu>
2023-12-02 17:53:02 -0800

    [leetcode] solve 34: search range

---
+++ leetcode/34.py
@@ -0,0 +1,27 @@
+"""LongestPanlindrome
+
+Algorithm : 
+    Binary Search
+Level :
+    Medium
+Status :
+    Accepted
+
+Sat Dec  2 17:52:35 PST 2023
+"""
+
+from bisect import bisect_left, bisect_right
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        result = []
+
+        left = bisect_left(nums, target)
+        right = bisect_right(nums, target)
+
+        if left == right:
+            result = [-1, -1]
+        else:
+            result = [left, right-1]
+
+        return result
