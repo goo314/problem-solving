991f8df550374ad564f5201106b1862c56fefbbe
goo314 <rn0731@g.skku.edu>
2023-12-03 16:41:03 -0800

    [leetcode] solve 746: min cost climbing stairs

---
+++ leetcode/746.py
@@ -0,0 +1,26 @@
+"""LongestPanlindrome
+
+Algorithm : 
+    DP
+Level :
+    Easy
+Status :
+    Accepted
+
+Sun Dec  3 16:40:37 PST 2023
+"""
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        result = 0
+
+        n = len(cost)
+        dp = [1000]*n
+        
+        dp[0], dp[1] = cost[0], cost[1]
+        for i in range(2, n):
+            dp[i] = min(dp[i-2]+cost[i], dp[i-1]+cost[i])
+
+        result = min(dp[n-2], dp[n-1])
+
+        return result
